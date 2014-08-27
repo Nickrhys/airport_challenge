0f73cb07ec04670d505f75d716864d34ceabb387
Nick Roberts <nickrhys@me.com>
2014-08-27 12:20:33 +0100

    airport class tested

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,31 @@
-# class Airport
+class Airport
+
+	# DEFAULT_CAPACITY = 10
+
+	def initialize
+		@planes = []
+		@capacity = 5
+
+	end
+
+	def plane_land(plane)
+		raise "Airport full" if full?
+		@planes << plane
+	end
+
+	def plane_count
+		@planes.count 
+	end
+
+	def plane_fly
+		@planes.pop
+	end
+
+	def full?
+		plane_count == @capacity
+	end
 
 
-# end
+
+
+end
